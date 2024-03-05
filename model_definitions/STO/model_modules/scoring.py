4f04031384a9476edb6929763922b1c8f640e9c3
Zadorozhniy, Anton <anton.zadorozhniy@teradata.com>
2024-03-05 12:38:55 +0100

    STO fixes

--- model_definitions/STO/model_modules/scoring.py
+++ model_definitions/STO/model_modules/scoring.py
@@ -3,57 +3,67 @@ from teradatasqlalchemy.types import INTEGER
 from collections import OrderedDict
 from .util import get_df_with_model
 from aoa.util import (
-	check_sto_version,
-	aoa_create_context,
-	ModelContext
+    check_sto_version,
+    aoa_create_context,
+    ModelContext,
+    execute_sql
 )
 
 import base64
 import dill
 
+
 def score(context: ModelContext, **kwargs):
 
-	aoa_create_context()
-	
-	model_version = context.model_version
-	model_table = "vmo_sto_models"
+    aoa_create_context()
 
-	check_sto_version()
+    model_version = context.model_version
+    model_table = "vmo_sto_models"
 
-	df = DataFrame.from_query(context.dataset_info.sql)
+    check_sto_version()
 
-	def score_partition(partition, features):
+    if model_version == "cli":
+        execute_sql(
+            f"DELETE FROM {context.dataset_info.predictions_table} WHERE job_id='cli'")
 
-		rows = partition.read()
+    df = DataFrame.from_query(context.dataset_info.sql)
 
-		if rows is None or len(rows) == 0:
-			return None
+    def score_partition(partition, features):
 
-		# the model artefact is available on the 1st row only (see how we joined in the dataframe query)
-		model_artefact = rows.loc[rows['n_row'] == 1, 'model'].iloc[0]
-		model = dill.loads(base64.b64decode(model_artefact))
+        rows = partition.read()
 
-		out_df = rows[["PatientId"]]
-		out_df["prediction"] = model.predict(rows[features])
+        if rows is None or len(rows) == 0:
+            return None
 
-		return out_df
-	
-	number_of_amps = 2
-	pdf = df.assign(partition_id = df.PatientId % number_of_amps)
-	partitioned_dataset_table = "partitioned_dataset"
-	pdf.to_sql(partitioned_dataset_table,if_exists ='replace',temporary=True)
+        # the model artefact is available on the 1st row only (see how we joined in the dataframe query)
+        model_artefact = rows.loc[rows['n_row'] == 1, 'model'].iloc[0]
+        model = dill.loads(base64.b64decode(model_artefact))
 
-	df_with_model = get_df_with_model(partitioned_dataset_table,model_table,model_version)
+        out_df = rows[["PatientId"]]
+        out_df["prediction"] = model.predict(rows[features])
 
-	features = ["NumTimesPrg", "Age", "PlGlcConc", "BloodP", "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc"]
+        return out_df
 
-	scored_df = df_with_model.map_partition(lambda partition: score_partition(partition, features), 
-										data_partition_column="partition_id",
-										returns=OrderedDict(
-										[('PatientId', INTEGER()),
-										('HasDiabetes', INTEGER())]))
-	
-	scored_df = scored_df.assign(job_id = context.job_id, json_report = None).select(['job_id', 'PatientId', 'HasDiabetes', 'json_report'])
-	scored_df.to_sql(context.dataset_info.predictions_table, if_exists="append")
+    number_of_amps = 2
+    pdf = df.assign(partition_id=df.PatientId % number_of_amps)
+    partitioned_dataset_table = "partitioned_dataset"
+    pdf.to_sql(partitioned_dataset_table, if_exists='replace', temporary=True)
 
-	print("Finished scoring")
+    df_with_model = get_df_with_model(
+        partitioned_dataset_table, model_table, model_version)
+
+    features = ["NumTimesPrg", "Age", "PlGlcConc", "BloodP",
+                "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc"]
+
+    scored_df = df_with_model.map_partition(lambda partition: score_partition(partition, features),
+                                            data_partition_column="partition_id",
+                                            returns=OrderedDict(
+        [('PatientId', INTEGER()),
+         ('HasDiabetes', INTEGER())]))
+
+    scored_df = scored_df.assign(job_id=context.job_id, json_report="").select(
+        ["job_id", "PatientId", "HasDiabetes", "json_report"])
+    scored_df.to_sql(context.dataset_info.predictions_table,
+                     if_exists="append")
+
+    print("Finished scoring")
--- model_definitions/STO/model_modules/training.py
+++ model_definitions/STO/model_modules/training.py
@@ -6,11 +6,11 @@ from sklearn.preprocessing import MinMaxScaler
 from collections import OrderedDict
 from aoa import ModelContext
 from aoa.util import (
-	save_metadata,
-	cleanup_cli,
-	check_sto_version,
-	collect_sto_versions,
-	aoa_create_context,
+    save_metadata,
+    cleanup_cli,
+    check_sto_version,
+    collect_sto_versions,
+    aoa_create_context,
 )
 
 import numpy as np
@@ -18,86 +18,88 @@ import json
 import base64
 import dill
 
+
 def train(context: ModelContext, **kwargs):
 
-	aoa_create_context()
+    aoa_create_context()
 
-	model_version = context.model_version
-	hyperparams = context.hyperparams
-	model_artefacts_table = "vmo_sto_models"
-	
-	check_sto_version()
+    model_version = context.model_version
+    hyperparams = context.hyperparams
+    model_artefacts_table = "vmo_sto_models"
 
-	cleanup_cli("cli")
+    check_sto_version()
 
-	# select the training datast via the fold_id
-	df = DataFrame.from_query(context.dataset_info.sql)
+    cleanup_cli("cli")
 
-	# perform simple feature engineering example using map_row
-	def transform_row(row):
-		row['Age'] = row['Age'] + 10
-		return row
+    # select the training datast via the fold_id
+    df = DataFrame.from_query(context.dataset_info.sql)
 
-	df = df.map_row(lambda row: transform_row(row))
+    # perform simple feature engineering example using map_row
+    def transform_row(row):
+        row['Age'] = row['Age'] + 10
+        return row
 
-	def train_partition_model(partition, model_version, hyperparams):
-		# read all of the rows into memory (we can also process in chunks)
-		rows = partition.read()
+    df = df.map_row(lambda row: transform_row(row))
 
-		# return if partition has no data
-		if rows is None or len(rows) == 0:
-			return None
-		
-		x = rows[["NumTimesPrg", "Age", "PlGlcConc", "BloodP", "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc"]]
-		y = rows[["HasDiabetes"]]
+    def train_partition_model(partition, model_version, hyperparams):
+        # read all of the rows into memory (we can also process in chunks)
+        rows = partition.read()
 
-		model = Pipeline([('scaler', MinMaxScaler()),
-						('xgb', XGBClassifier(eta=hyperparams["eta"], max_depth=hyperparams["max_depth"]))])
-		
-		model.fit(x, y)
-		
-		partition_id = rows.partition_id.iloc[0]
+        # return if partition has no data
+        if rows is None or len(rows) == 0:
+            return None
 
-		partition_metadata = json.dumps({
-			"num_rows": rows.shape[0],
-			"hyper_parameters": hyperparams
-		})
-		
-		# we have to convert the model to base64 to store in a CLOB column (can't use BLOB with STOs)
-		artefact = base64.b64encode(dill.dumps(model))
-		
-		# here we return 1 row per partition - basically, the trained model for that partitiom
-		return np.array([[partition_id,
-						model_version, 
-						rows.shape[0], 
-						partition_metadata,
-						artefact]])
-	
-	print("Starting training...")
+        x = rows[["NumTimesPrg", "Age", "PlGlcConc", "BloodP",
+                  "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc"]]
+        y = rows[["HasDiabetes"]]
 
-	number_of_amps = 2
-	pdf = df.assign(partition_id = df.PatientId % number_of_amps)
-	partitioned_dataset_table = "partitioned_dataset"
-	pdf.to_sql(partitioned_dataset_table,if_exists ='replace',temporary=True)
+        model = Pipeline([('scaler', MinMaxScaler()),
+                          ('xgb', XGBClassifier(eta=hyperparams["eta"], max_depth=hyperparams["max_depth"]))])
 
-	train_df = DataFrame('partitioned_dataset')
+        model.fit(x, y)
 
-	model_df = train_df.map_partition(lambda partition: train_partition_model(partition, model_version, hyperparams),
-						data_partition_column="partition_id",
-						returns=OrderedDict(
-										[('partition_id', VARCHAR(255)),
-										('model_version', VARCHAR(255)),
-										('num_rows', INTEGER()),
-										('partition_metadata', CLOB()),
-										('model_artefact', CLOB())]))
-										
-								
-	model_df.to_sql(model_artefacts_table, if_exists="replace")
-	model_df = DataFrame(query=f"SELECT * FROM {model_artefacts_table} WHERE model_version='{model_version}'")
-	
-	save_metadata(model_df)
+        partition_id = rows.partition_id.iloc[0]
 
-	print("Finished training")
+        partition_metadata = json.dumps({
+            "num_rows": rows.shape[0],
+            "hyper_parameters": hyperparams
+        })
 
-	with open(f"{context.artifact_output_path}/sto_versions.json", "w+") as f:
-		json.dump(collect_sto_versions(), f)
+        # we have to convert the model to base64 to store in a CLOB column (can't use BLOB with STOs)
+        artefact = base64.b64encode(dill.dumps(model))
+
+        # here we return 1 row per partition - basically, the trained model for that partitiom
+        return np.array([[partition_id,
+                          model_version,
+                          rows.shape[0],
+                          partition_metadata,
+                          artefact]])
+
+    print("Starting training...")
+
+    number_of_amps = 2
+    pdf = df.assign(partition_id=df.PatientId % number_of_amps)
+    partitioned_dataset_table = "partitioned_dataset"
+    pdf.to_sql(partitioned_dataset_table, if_exists='replace', temporary=True)
+
+    train_df = DataFrame('partitioned_dataset')
+
+    model_df = train_df.map_partition(lambda partition: train_partition_model(partition, model_version, hyperparams),
+                                      data_partition_column="partition_id",
+                                      returns=OrderedDict(
+        [('partition_id', VARCHAR(255)),
+         ('model_version', VARCHAR(255)),
+         ('num_rows', INTEGER()),
+         ('partition_metadata', CLOB()),
+         ('model_artefact', CLOB())]))
+
+    model_df.to_sql(model_artefacts_table, if_exists="replace")
+    model_df = DataFrame(
+        query=f"SELECT * FROM {model_artefacts_table} WHERE model_version='{model_version}'")
+
+    save_metadata(model_df)
+
+    print("Finished training")
+
+    with open(f"{context.artifact_output_path}/sto_versions.json", "w+") as f:
+        json.dump(collect_sto_versions(), f)
